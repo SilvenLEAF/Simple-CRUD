82cc8e5294cf6b4b7751901d3fc8ab0c1b7e1d04
Manash Sarma <SilvenLEAF@gmail.com>
2020-06-24 11:57:03 +0530

    FrontEND fetch GET and POST

---
+++ client/scriptsFOL/main.js
@@ -0,0 +1,50 @@
+//--------data
+const nameOutput = document.querySelector('#name-output');
+const messageOutput = document.querySelector('#message-output');
+
+
+
+//--------forms
+const getForm = document.querySelector('#get-form');
+const postForm = document.querySelector('#post-form');
+
+
+
+//---form inputs
+const postNameInput = document.querySelector('#post-name-input');
+const postMessageInput = document.querySelector('#post-message-input');
+
+
+postForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const postNameValue = postNameInput.value;
+    const postMessageValue = postMessageInput.value;
+
+    fetch('/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: postNameValue,
+            message: postMessageValue
+        })
+    }).then( res =>{
+        return res.json();
+    }).then( data =>{
+        console.log(data);
+    }).catch( err =>{
+        console.log(err);
+    })
+});
+
+getForm.addEventListener('submit', ()=>{
+    fetch('/api').then(res =>{
+        return res.json();
+    }).then(data =>{
+        console.log(data);
+    }).catch( err =>{
+        console.log(err);
+    })
+})
